29b17235a5d34c3f5c126c3b90d7b2165d9c3182
Wenxin Liu <wliuad@connect.ust.hk>
2016-02-17 00:49:25 +0800

    modified xml

--- src/fyp_3d_reconstruction/src/main.cpp
+++ src/fyp_3d_reconstruction/src/main.cpp
@@ -6,9 +6,14 @@
 using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
     cout << "All good! :)" << endl;
+    
+    Mat Img;
+
     return 0;
 }
 
+
+// To test the program in devel space, go to <catkin_ws>/devel/lib/fyp_3d_reconstruction
