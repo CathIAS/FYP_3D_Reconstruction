fc3c4f922332a9b2f735d29fd35055be79c832fe
Wenxin Liu <wliuad@connect.ust.hk>
2016-02-16 23:58:55 +0800

    environment with ROS and OpenCV set

---
+++ src/fyp_3d_reconstruction/src/main.cpp
@@ -0,0 +1,14 @@
+#include "ros/ros.h"
+#include "opencv2/opencv.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout << "All good! :)" << endl;
+    return 0;
+}
+
