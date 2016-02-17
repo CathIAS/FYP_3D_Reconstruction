add0a712d7c1f4e06c117639f1d8eb96087feb5e
Wenxin Liu <wliuad@connect.ust.hk>
2016-02-18 01:52:18 +0800

    added instructions for next step, deleted unecessary folders.

--- fyp_3d_reconstruction/src/main.cpp
+++ fyp_3d_reconstruction/src/main.cpp
@@ -1,17 +1,26 @@
 #include "ros/ros.h"
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
 #include <dirent.h>  // for reading files from path
 #include <sys/stat.h>
 #include <sstream>  // string stream
-#include <algorithm>  // include sorting algorithm
+#include <algorithm>  // sorting algorithm
 
 using namespace cv;
 using namespace std;
 
-/* Returns a list of files in a directory (except the ones that begin with a dot) */
 
+/***********************************************/
+
+/*                 Functions                   */
+
+/***********************************************/
+
+/* TODO Set cam parameters */
+
+/* Returns a list of files in a directory (except the ones that begin with a dot) */
 void readFilenames(vector<string> &filenames, const string &directory)
 {
     DIR *dir;
@@ -43,35 +52,36 @@ void readFilenames(vector<string> &filenames, const string &directory)
 
 /***********************************************/
 
+/*                    Main                     */
+
+/***********************************************/
+
 int main(int argc, char** argv)
 {
     cout << "Ready!" << endl;
-//    cout << "Number of arguments: " << argc << endl;
-//    cout << "argv[1] (mode): " << argv[1] << endl;
 
     /* Specifies executable usage */
-
     if(argc != 3)
     {
         cout << "Usage: ./fyp_3d_reconstruction <mode> [directory]" << endl;
         cout << "       <mode> : images" << endl;
-        cout << "       [directory] : path to image folder. Do NOT add '/' at the end." << endl;
+        cout << "       [directory] : path to image folder. Do NOT add '/' at the end. Folder should only contain image files" << endl;
         return -1;
     }
 
+    /* In "images" mode - reading images from folder */
     if (strcmp(argv[1], "images") == 0)  // if argv[1] is "images"
     {
         cout << "Entering mode: images" << endl;
 
         /* Read in images from folder path */
-        
         const string folder = argv[2];  // get folder path from command line
         cout << "Folder path: " << argv[2] << endl;
 
-        vector<string> filenames;  // initialize vector of filenames
+        vector<string> filenames;  // a vector of the found names in the path
 
-        readFilenames(filenames, folder); // get filenames from folder
-        sort(filenames.begin(), filenames.end()); // sort filenames
+        readFilenames(filenames, folder);  // get names and assign them to vector
+        sort(filenames.begin(), filenames.end());  // sort filenames
         cout << "images discovered under folder: " << endl;
         for (int i=0; i<filenames.size(); i++)
             cout << "   " << filenames[i] << endl;
@@ -88,22 +98,25 @@ int main(int argc, char** argv)
                 cerr << "Problem loading image!" << endl;
                 return -1;
             }
-            
-            /* Turn int to string */
-            stringstream ss;
-            ss << i+1;
+             
             /* Show image */
-            namedWindow("Image"+ss.str(), WINDOW_NORMAL);
-            imshow("Image"+ss.str(),img[i]);
+            stringstream ss;  // turn int to string
+            ss << i+1;
+            namedWindow("Image"+ss.str(), WINDOW_NORMAL);  // adjust display window size
+            imshow("Image"+ss.str(),img[i]);  // show image in the window. "Image1"
             waitKey();
-            }
+        }
+
+        /* TODO Undistortion with cam parameters */
+        /* TODO SURF detector for features */
+
+        /* THINK: how to arrange the code to do matching, as the match needs to be done 2 by 2 in order. (k-1, k) > (k, k+1) > ... It might be best to write SURF and matching as individual functions that can be called in main. */
 
     }
-
-
 
     return 0;
 }
 
 
 //  Program executable is at <catkin_ws>/devel/lib/fyp_3d_reconstruction
+//  cmd example: ./fyp_3d_reconstruction images ~/photos
