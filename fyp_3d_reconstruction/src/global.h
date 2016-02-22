e441ae4f1273857fdb5151bf71575fd3dab48cfe
Xuelai <sherry20091160@gmail.com>
2016-02-23 00:47:20 +0800

    surf

--- fyp_3d_reconstruction/src/global.h
+++ fyp_3d_reconstruction/src/global.h
@@ -1,4 +1,4 @@
-#include "opencv2/core/core.hpp"
+#include "opencv2/core.hpp"
 
 using namespace cv;
 
--- fyp_3d_reconstruction/src/surf.cpp
+++ fyp_3d_reconstruction/src/surf.cpp
@@ -1,50 +1,64 @@
-#include <stdio.h>
-#include <iostream>
-#include "opencv2/core.hpp"
-#include "opencv2/features2d.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/xfeatures2d.hpp"
-
-using namespace cv;
-using namespace cv::xfeatures2d;
-
-void readme();
-
-int surf( int argc, char** argv )
+#include "surf.h"
+void surf(Mat img_undist[],vector< DMatch >& good_matches)
 {
-      if( argc != 3 )
-      { readme(); return -1; }
+			vector<KeyPoint> keypoints_1,keypoints_2;
+			Mat gimg_1;
+            Mat gimg_2;
+            cv::cvtColor(img_undist[7], gimg_1, cv::COLOR_BGR2GRAY);            
+            cv::cvtColor(img_undist[8], gimg_2, cv::COLOR_BGR2GRAY);            
 
-            Mat img_1 = imread( argv[1], IMREAD_GRAYSCALE );
-            Mat img_2 = imread( argv[2], IMREAD_GRAYSCALE );
-
-            if( !img_1.data || !img_2.data )
-                  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+            if( !gimg_1.data || !gimg_2.data )
+                  { std::cout<< " --(!) Error reading images " << std::endl;  }
 
               //-- Step 1: Detect the keypoints using SURF Detector
             int minHessian = 400;
               
-                  Ptr<SURF> detector = SURF::create( minHessian );
+            Ptr<SURF> detector = SURF::create( minHessian );
+			detector->detect( gimg_1, keypoints_1);
+			detector->detect( gimg_2, keypoints_2);
+            
+            
+            Mat descriptors_1, descriptors_2;    
+			detector->compute( gimg_1, keypoints_1, descriptors_1 );
+			detector->compute( gimg_2, keypoints_2, descriptors_2 );  
+
+            //-- Step 3: Matching descriptor vectors using FLANN matcher
+			FlannBasedMatcher matcher;
+			std::vector< DMatch > matches;
+			matcher.match( descriptors_1, descriptors_2, matches );
+			
+			double max_dist = 0; double min_dist = 100;
+
+			//-- Quick calculation of max and min distances between keypoints
+			for( int i = 0; i < descriptors_1.rows; i++ )
+			{ double dist = matches[i].distance;
+			if( dist < min_dist ) min_dist = dist;
+			if( dist > max_dist ) max_dist = dist;
+			}
+
+			printf("-- Max dist : %f \n", max_dist );
+			printf("-- Min dist : %f \n", min_dist );
+            
+        /*  //-- Draw matches
+			Mat img_matches;
+			drawMatches( gimg_1, keypoints_1, gimg_2, keypoints_2, matches, img_matches );
+		*/
+			
+			//-- Draw only "good" matches
+			  for( int i = 0; i < descriptors_1.rows; i++ )
+			{ if( matches[i].distance <= max(2*min_dist, 0.02) )
+				{ good_matches.push_back( matches[i]); }
+			}
+			
+			Mat img_matches;
+			drawMatches( gimg_1, keypoints_1, gimg_2, keypoints_2,good_matches, img_matches);
+
+			//-- Show detected matches
+			
+			namedWindow("Matches", CV_WINDOW_NORMAL);
+			imshow("Matches", img_matches);
+			
+           waitKey();
               
-                    std::vector<KeyPoint> keypoints_1, keypoints_2;
-              
-                      detector->detect( img_1, keypoints_1 );
-                        detector->detect( img_2, keypoints_2 );
-              
-                //-- Draw keypoints
-                            Mat img_keypoints_1; Mat img_keypoints_2;
-              
-                              drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-                                drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-              
-                //-- Show detected (drawn) keypoints
-                                    imshow("Keypoints 1", img_keypoints_1 );
-                                      imshow("Keypoints 2", img_keypoints_2 );
-              
-                                        waitKey(0);
-              
-                                          return 0;
 }
 
-void readme()
-      { std::cout << " Usage: ./SURF_detector <img1> <img2>" << std::endl; }
