9b0a060fbb3e0346490999f129822728593cadab
Xuelai <sherry20091160@gmail.com>
2016-04-17 22:06:52 +0800

    first merge done

---
+++ fyp_3d_reconstruction/include/bundle.h
@@ -0,0 +1,67 @@
+#ifndef BUNDLE_H
+#define BUNDLE_H
+
+#include "global.h"
+
+#include "opencv2/core.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <ctime>
+
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <Eigen/OrderingMethods>
+#include <opencv2/core/eigen.hpp>
+
+
+// convert from pixel fraame to image frame using camera matrix
+cv::Point2f pix2img(cv::Point2f pt_pix);
+
+// projection function Jacobian 2x3
+Eigen::MatrixXf cal_Jp( const Eigen::Matrix3f R, const Eigen::Vector3f T, const Eigen::Vector3f X);
+
+// complete Jacobian of theta, translation and landmark coordinates 2x3
+Eigen::MatrixXf cal_Jt( const Eigen::Matrix3f R, const Eigen::Vector3f T, const Eigen::Vector3f X);
+Eigen::MatrixXf cal_Jr( const Eigen::Matrix3f R, const Eigen::Vector3f T, const Eigen::Vector3f X);
+Eigen::MatrixXf cal_Jx( const Eigen::Matrix3f R, const Eigen::Vector3f T, const Eigen::Vector3f X);
+
+// convert variables to Eigen library
+void toEigen( const cv::Mat R[], const cv::Mat T[], const cv::Mat theta[], const std::vector<cv::Point3f> pts3, Eigen::Matrix3f Rot[], Eigen::Vector3f Theta[], Eigen::Vector3f Tra[], Eigen::Vector3f P3[], int m, int n);
+
+// convert variables to OpenCV library
+void toCV( cv::Mat R[], cv::Mat T[], cv::Mat theta[], std::vector<cv::Point3f>& pts3, const Eigen::Matrix3f Rot[], const Eigen::Vector3f Theta[], const Eigen::Vector3f Tra[], const Eigen::Vector3f P3[], int m, int n);
+
+// get reprojections in image frame
+std::vector< std::vector<cv::Point2f> >  getReprojection( const Eigen::Vector3f Theta[], const Eigen::Vector3f Tra[], const Eigen::Vector3f P3[], int m, int n);
+
+// get observations in image frame
+std::vector< std::vector<cv::Point2f> >  getObservation( const std::vector< std::vector<cv::Point2f> >& z, int m, int n);
+
+// update error vector
+void updateError( const std::vector< std::vector<cv::Point2f> >& z, const std::vector< std::vector<cv::Point2f> >& rep, const std::vector< std::vector<cv::Point2f> >& z_img, const int m_cam, const int n_pts, Eigen::VectorXf& err);
+
+// form Jacobian Matrix
+void getJacobian( const std::vector< std::vector<cv::Point2f> >& z, const Eigen::Matrix3f Rot[], const Eigen::Vector3f Tra[], const Eigen::Vector3f P3[], int m_cam, int n_pts, Eigen::MatrixXf& J);
+
+// form Jacobian and Hessian by block insertion
+void getJnH( const std::vector< std::vector<cv::Point2f> >& z, const Eigen::Matrix3f Rot[], const Eigen::Vector3f Tra[], const Eigen::Vector3f P3[], int m_cam, int n_pts, Eigen::SparseMatrix<float>& J, Eigen::SparseMatrix<float>& H);
+
+// increment delta x to all the variables
+void increX( const Eigen::VectorXf& del_x, Eigen::Vector3f Theta[], Eigen::Matrix3f Rot[], Eigen::Vector3f Tra[], Eigen::Vector3f P3[], int m_cam, int n_pts);
+
+// Bundle Adjustment main function
+void bundle(cv::Mat R[], cv::Mat T[], std::vector<cv::Point3f>& pts3, const std::vector< std::vector<cv::Point2f> >& z, int m_cam, int n_pts);
+
+// Cam - K: camera matrix, R[]: rotation matrices, T[]: translations
+// Points - points3D (vector)
+// Relation - points2D (vector of vector), (-1,-1) if cannot see from that cam
+// m_cam - number of cameras
+// n_pts - number of 3D points
+
+#endif /* BUNDLE_H */
--- fyp_3d_reconstruction/include/wrapper.h
+++ fyp_3d_reconstruction/include/wrapper.h
@@ -13,22 +13,25 @@
 #include "opencv2/core.hpp"
 #include <opencv2/imgproc.hpp>
 #include <opencv2/calib3d.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/core/eigen.hpp>
 
 #include "global.h"
 
 void undistort(const cv::Mat img[], cv::Mat img_undist[], const cv::Mat cameraMatrix, const cv::Mat distCoeffs);
 
-void findEssentialMat(const std::vector< cv::Point2f > points_1[],const std::vector< cv::Point2f > points_2[],
-						const cv::Mat cameraMatrix, int method, double prob,
-						double threshold, cv::Mat mask[], cv::Mat E[]);
 
-void recoverPose(const cv::Mat E[], const std::vector< cv::Point2f > points_1[],const std::vector< cv::Point2f > points_2[],
-				const cv::Mat cameraMatrix, cv::Mat R[], cv::Mat t[],cv::Mat mask[]);
+void r2q(cv::Mat R[],Eigen::Quaterniond& q0,Eigen::Quaterniond q[]);
 
-void r2q(cv::Mat R[],Eigen::Quaterniond q0,Eigen::Quaterniond q[]);
+void triangulate_init(cv::Mat R, cv::Mat t,const std::vector< cv::Point2f > points_1,
+						const std::vector< cv::Point2f > points_2,cv::Mat& points4D,std::vector< cv::Point2f > mask3D[]);
 
-void triangulatePoints( const cv::Mat R[],const cv::Mat t[],const std::vector< cv::Point2f > points[],
-						const std::vector< cv::Point2f > pointsCompare[],cv::Mat points4D[] );
+void add_Points( cv::Mat R[], cv::Mat t[],const std::vector< cv::Point2f > points_1[],
+        const std::vector< cv::Point2f > points_2[],cv::Mat& points3D,const int add,std::vector< cv::Point2f > mask3D[],cv::Mat& img_matches);
+
+void PnP(const std::vector< cv::DMatch > good_matches[],const int q,const std::vector<cv::KeyPoint> keypoints[], cv::Mat R[],
+		cv::Mat t[],std::vector< cv::Point2f > points_1[], std::vector< cv::Point2f > points_2[],std::vector< cv::Point2f > mask3D[],cv::Mat& img_matches);
+
+void invertpose(const cv::Mat R[],cv::Mat t[],cv:: Mat _R[], cv::Mat _t[] );
 
 #endif /* FYP_3D_RECONSTRUCTION_INCLUDE_WRAPPER_H_ */
